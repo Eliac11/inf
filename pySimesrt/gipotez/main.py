a8d6865fcd661af73c4eaa2cf4d673b1b39587e9
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2023-03-21 23:00:48 +0300

    rewrite gipotez

--- pySimesrt/gipotez/main.py
+++ pySimesrt/gipotez/main.py
@@ -1,11 +1,17 @@
 from ctypes import *
-
+# импорт библиотеки из лабы на плюсах
 cal = CDLL('./libtest.so')
+
+# ввод количества чисел для вывода и создания массива этой длины
 m = [0]*int(input("Input N: "))
 
+#  Создание Си подобного массива
 c_m = (c_int * len(m))(*m)
+
+# Поиск чисел
 cal.calculate_primes(c_m, len(m))
 
+# вывод всех простых чисел
 for i,r in enumerate(c_m):
     if r:
         print(i)

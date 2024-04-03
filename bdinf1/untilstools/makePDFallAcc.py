1cb6b0e01db02a429ea980d5f56c9d2cc8720a39
Ilya Alexseevich <58089493+Eliac11@users.noreply.github.com>
2024-04-03 23:59:20 +0300

    fix reports

--- bdinf1/untilstools/makePDFallAcc.py
+++ bdinf1/untilstools/makePDFallAcc.py
@@ -57,7 +57,7 @@ def create_reportAllAcc():
         table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
                                    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
                                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                                   ('FONTNAME', (0, 0), (-1, 0), 'DudkaRegular'),
+                                   ('FONTNAME', (0, 0), (-1, -1), 'DudkaRegular'),
                                    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
                                    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
                                    ('GRID', (0, 0), (-1, -1), 1, colors.black)]))
--- bdinf1/untilstools/makePDFendAcc.py
+++ bdinf1/untilstools/makePDFendAcc.py
@@ -59,7 +59,7 @@ def create_reportEndAcc():
         operations_table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.gray),
                                             ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
                                             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                                            ('FONTNAME', (0, 0), (-1, 0), 'DudkaRegular'),
+                                            ('FONTNAME', (0, 0), (-1, -1), 'DudkaRegular'),
                                             ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
                                             ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
                                             ('GRID', (0, 0), (-1, -1), 1, colors.black)]))

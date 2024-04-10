50c818bd27d5f8476eb0a993e7fc5da20ea61db2
Ilya Alexseevich <58089493+Eliac11@users.noreply.github.com>
2024-04-10 12:29:23 +0300

    add event operation

--- bdinf1/dbtools/database.py
+++ bdinf1/dbtools/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from toolenv import get_dbconf
 
+from dbtools.models import tblClient, tblAccountType, tblAccount, tblOperationType, tblOperation
 
 
 
@@ -20,3 +21,20 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+#EVANTS 
+
+def on_operation_insert(mapper, connection, operation):
+    session = sessionmaker(bind=connection)()
+    account_id = operation.intAccountId
+    account = session.query(tblAccount).filter_by(intAccountId=account_id).first()
+    if account:
+        account.fltAccountSum += operation.fltValue
+        session.commit()
+    session.close()
+
+# Подписываемся на событие добавления операции
+event.listen(tblOperation, 'after_insert', on_operation_insert)
+
+
